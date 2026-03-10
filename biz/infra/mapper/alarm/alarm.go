97358b5a941baabcee485a76b970585f34445cdd
moonlight <10245101492@ecnu.edu.cn>
2026-03-10 10:02:26 +0800

    style:json小驼峰命名，bson蛇形命名，常量与coreapi统一

--- biz/infra/mapper/alarm/alarm.go
+++ biz/infra/mapper/alarm/alarm.go
@@ -17,13 +17,13 @@ var (
 
 type Alarm struct {
 	ID             bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	UserID         bson.ObjectID `json:"userId,omitempty" bson:"userId,omitempty"`
-	ReportID       bson.ObjectID `json:"reportId,omitempty" bson:"reportId,omitempty"`
-	ConversationID bson.ObjectID `json:"conversationId,omitempty" bson:"conversationId,omitempty"`
-	UnitID         bson.ObjectID `json:"unitId,omitempty" bson:"unitId,omitempty"`
+	UserID         bson.ObjectID `json:"userId,omitempty" bson:"user_id,omitempty"`
+	ReportID       bson.ObjectID `json:"reportId,omitempty" bson:"report_id,omitempty"`
+	ConversationID bson.ObjectID `json:"conversationId,omitempty" bson:"conversation_id,omitempty"`
+	UnitID         bson.ObjectID `json:"unitId,omitempty" bson:"unit_id,omitempty"`
 	Emotion        int32         `json:"emotion,omitempty" bson:"emotion,omitempty"`
 	Keywords       []string      `json:"keywords,omitempty" bson:"keywords,omitempty"`
 	Status         int32         `json:"status,omitempty" bson:"status,omitempty"`
-	CreateTime     time.Time     `json:"createTime,omitempty" bson:"createTime,omitempty"`
-	UpdateTime     time.Time     `json:"updateTime,omitempty" bson:"updateTime,omitempty"`
+	CreateTime     time.Time     `json:"createTime,omitempty" bson:"create_time,omitempty"`
+	DeleteTime     time.Time     `json:"updateTime,omitempty" bson:"update_time,omitempty"`
 }
--- biz/infra/mapper/report/report.go
+++ biz/infra/mapper/report/report.go
@@ -9,27 +9,27 @@ import (
 
 type Report struct {
 	// 基本信息
-	ID             bson.ObjectID          `bson:"_id,omitempty" json:"id,omitempty"`                        // 报表id
-	UnitID         bson.ObjectID          `bson:"unitId,omitempty" json:"unitId,omitempty"`                 // 单位id
-	UserID         bson.ObjectID          `bson:"userId,omitempty" json:"userId,omitempty"`                 // 用户id
-	ConversationID bson.ObjectID          `bson:"conversationId,omitempty" json:"conversationId,omitempty"` // 对话id
-	ReportUsage    *core.LLMUsage         `bson:"reportUsage" json:"reportUsage,omitempty"`                 // 报表生成总消耗
-	ChatUsage      *core.LLMUsage         `bson:"chatUsage" json:"chatUsage,omitempty"`                     // 大模型总token消耗
-	ASRUsage       *core.ASRUsage         `bson:"asrUsage,omitempty" json:"asrUsage,omitempty"`             // asr消耗
-	TTSUsage       *core.TTSUsage         `bson:"ttsUsage,omitempty" json:"ttsUsage,omitempty"`             // tts消耗
-	Round          int                    `bson:"round" json:"round"`                                       // 总轮数
-	Start          time.Time              `bson:"start" json:"start"`                                       // 对话开始时间
-	End            time.Time              `bson:"end" json:"end"`                                           // 对话结束时间
-	Config         *core.Config           `bson:"config" json:"config,omitempty"`                           // 对话配置
-	Info           map[string]interface{} `bson:"info" json:"info,omitempty"`                               // 额外信息
+	ID             bson.ObjectID          `bson:"_id,omitempty" json:"id,omitempty"`                         // 报表id
+	UnitID         bson.ObjectID          `bson:"unit_id,omitempty" json:"unitId,omitempty"`                 // 单位id
+	UserID         bson.ObjectID          `bson:"user_id,omitempty" json:"userId,omitempty"`                 // 用户id
+	ConversationID bson.ObjectID          `bson:"conversation_id,omitempty" json:"conversationId,omitempty"` // 对话id
+	ReportUsage    *core.LLMUsage         `bson:"report_usage" json:"reportUsage,omitempty"`                 // 报表生成总消耗
+	ChatUsage      *core.LLMUsage         `bson:"chat_usage" json:"chatUsage,omitempty"`                     // 大模型总token消耗
+	ASRUsage       *core.ASRUsage         `bson:"asr_usage,omitempty" json:"asrUsage,omitempty"`             // asr消耗
+	TTSUsage       *core.TTSUsage         `bson:"tts_usage,omitempty" json:"ttsUsage,omitempty"`             // tts消耗
+	Round          int                    `bson:"round" json:"round"`                                        // 总轮数
+	Start          time.Time              `bson:"start" json:"start"`                                        // 对话开始时间
+	End            time.Time              `bson:"end" json:"end"`                                            // 对话结束时间
+	Config         *core.Config           `bson:"config" json:"config,omitempty"`                            // 对话配置
+	Info           map[string]interface{} `bson:"info" json:"info,omitempty"`                                // 额外信息
 
 	// 报表结果
-	Title     string   `bson:"title" json:"title"`                   // 报表标题
-	Keywords  []string `bson:"keywords" json:"keywords,omitempty"`   // 关键词
-	Digest    string   `bson:"digest" json:"digest,omitempty"`       // 对话摘要
-	Emotion   string   `bson:"emotion" json:"emotion,omitempty"`     // 用户情绪状态
-	Body      string   `bson:"body" json:"body,omitempty"`           // 正文
-	NeedAlarm bool     `bson:"needAlarm" json:"needAlarm,omitempty"` // 是否需要创建预警
+	Title     string   `bson:"title" json:"title"`                    // 报表标题
+	Keywords  []string `bson:"keywords" json:"keywords,omitempty"`    // 关键词
+	Digest    string   `bson:"digest" json:"digest,omitempty"`        // 对话摘要
+	Emotion   string   `bson:"emotion" json:"emotion,omitempty"`      // 用户情绪状态
+	Body      string   `bson:"body" json:"body,omitempty"`            // 正文
+	NeedAlarm bool     `bson:"need_alarm" json:"needAlarm,omitempty"` // 是否需要创建预警
 }
 
 // Deprecated
